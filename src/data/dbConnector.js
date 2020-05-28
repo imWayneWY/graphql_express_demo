368d0bf3fee7e419da3ca16ba927e5eb203ae3db
weiyan <im.wayne.wy@gmail.com>
2020-05-28 10:50:20 -0600

    simple graphql demo

---
+++ src/data/dbConnector.js
@@ -0,0 +1,25 @@
+import mongoose from 'mongoose';
+
+// Mongo connection
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/contacts', {
+  useNewUrlParser: true,
+});
+
+
+const contactSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+  },
+  lastName: {
+    type: String,
+  },
+  email: {
+    type: String
+  },
+  company: {
+    type: String
+  }
+})
+
+export const Contacts = mongoose.model('contacts', contactSchema)
